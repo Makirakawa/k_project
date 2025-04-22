fb835b6b24c2b2582b13c3c2d9506940785938df
Makirakawa <164365524+Makirakawa@users.noreply.github.com>
2025-04-23 00:42:29 +0500

    add
    add

--- Registration.py
+++ Registration.py
@@ -1,17 +1,27 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Face check | Registraion"
-    page.window_width = 350
-    page.window_height = 550
-    page.window_resizable = False
+    #page.title = "Face check | Registraion"
+    page.window.width = 450
+    page.window.height = 650
+    page.window.resizable = False
     page.bgcolor = "#1F1F1F"  
-
+    page.window.title_bar_hidden = True
+    page.window.title_bar_buttons_hidden = True
 
     input_bg = "#333333"
-    hint_color = "#B0B0B0"
+    hint_color = "#00AAFF"
     text_color = "#FFFFFF"
     accent = "#3498db"
+
+    page.add(
+        ft.Row(
+            [
+                ft.WindowDragArea(ft.Container(ft.Text("Face check | Registraion"), padding=5), expand=True),
+                ft.IconButton(ft.Icons.CLOSE, on_click=lambda _: page.window.close())
+            ]
+        )
+    )
 
 
     login_input = ft.TextField(label="Логин", bgcolor=input_bg, border_color=accent, color=text_color)
@@ -19,21 +29,20 @@ def main(page: ft.Page):
     password_input = ft.TextField(label="Пароль", password=True, can_reveal_password=True, bgcolor=input_bg, border_color=accent, color=text_color)
     repeat_password_input = ft.TextField(label="Повторите пароль", password=True, can_reveal_password=True, bgcolor=input_bg, border_color=accent, color=text_color)
 
-    register_button = ft.ElevatedButton("ЗАРЕГИСТРИРОВАТЬСЯ", bgcolor=accent, color="white", height=50)
+    register_button = ft.ElevatedButton("ЗАРЕГИСТРИРОВАТЬСЯ", bgcolor=hint_color, color="white", height=50, width=300)
 
-    login_text = ft.Text("Уже есть аккаунт? ", color=hint_color, size=13)
+    login_text = ft.Text("Уже есть аккаунт? ", color=text_color, size=13)
     login_link = ft.Text("Войти", color=accent, size=13, weight="bold")
-
-
-    logo = ft.Image(
-        src="https://img.icons8.com/ios-filled/100/user.png",
-        width=80,
-        height=80,
-    )
+    
+    # logo = ft.Image(
+    #     src="C:\Users\m3ddi\Desktop\k_reg\logo\ic_baseline-face.png",
+    #     width="80",
+    #     height="80",
+    #     )
 
     content = ft.Column(
         controls=[
-            ft.Container(content=logo, alignment=ft.alignment.center, padding=20),
+            ft.Container( alignment=ft.alignment.center, padding=20),
             login_input,
             email_input,
             password_input,
